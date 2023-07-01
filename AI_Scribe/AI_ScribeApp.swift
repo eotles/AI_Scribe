2b2b27343fdaf1cc1114577222bd8f7e5eeee3e0
Erkin Otles <eotles@gmail.com>
2023-06-30 21:12:10 -0500

    Initial Commit

---
+++ AI_Scribe/AI_ScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ScribeApp.swift
+//  AI_Scribe
+//
+//  Created by Erkin Otles on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_ScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
